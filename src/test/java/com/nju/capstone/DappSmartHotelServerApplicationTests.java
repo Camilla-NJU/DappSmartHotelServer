b4ae037718b6d97ca9bf5c23044462b6f901e294
Camilla <1095279125728@tianchi.com>
2018-04-10 00:09:21 +0800

    create repository

---
+++ src/test/java/com/nju/capstone/DappSmartHotelServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nju.capstone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DappSmartHotelServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
